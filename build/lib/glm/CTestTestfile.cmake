6b09c777a1988e0714743dcedc51bfc82ba4f679
drunkard <51318605+Lir0sis@users.noreply.github.com>
2023-05-06 16:13:21 +0300

    deleted build/ dir

--- build/lib/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Valentyn/Desktop/Diploma/AnimeTest/lib/glm
-# Build directory: C:/Users/Valentyn/Desktop/Diploma/AnimeTest/build/lib/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
